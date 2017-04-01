f6209d81febc7cd51118de04fe91c1253ed5bd79
mmts1007 <kira_yamat0518@yahoo.co.jp>
2017-04-01 23:15:18 +0900

    Support Ubuntu

--- cookbooks/rails/dependency.rb
+++ cookbooks/rails/dependency.rb
@@ -1,8 +1,16 @@
-package 'sqlite-devel'
-package 'epel-release'
-package 'nodejs' do
-  options '--enablerepo=epel'
-end
-package 'npm' do
-  options '--enablerepo=epel'
+case node[:platform]
+when 'debian', 'ubuntu'
+  package 'sqlite3'
+  package 'libsqlite3-dev'
+  package 'npm'
+when 'redhat'
+  package 'sqlite-devel'
+  package 'epel-release'
+  package 'nodejs' do
+    options '--enablerepo=epel'
+  end
+  package 'npm' do
+    options '--enablerepo=epel'
+  end
+else
 end
